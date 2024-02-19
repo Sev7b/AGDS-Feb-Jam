900cadfc9b6f43d2302574d8773707d6642726cc
Sev-B <lorns.allen@gmail.com>
2024-02-19 21:03:54 +0000

    Added Cinemachine package and created a settings and player controller script

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    // These variables can be accessed without being attached to a Game Object because they are static, you can get or set any variable with "Settings.variableName;"
+    public static float masterVolume;
+}
