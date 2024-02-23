d013652a52a4f7bd1dc21b41436c00e3415a767a
Sev-B <lorns.allen@gmail.com>
2024-02-23 12:31:25 +0000

    Update Main Menu Graphics and added bullet merging

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,19 +4,53 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public GameObject newBulletPrefab;
+
     public int playerSource;
     [HideInInspector] public int damage;
     [HideInInspector] public float speed;
+    public Vector2 direction; 
+
+    void Start()
+    {
+        direction = GetComponent<Rigidbody2D>().velocity;
+    }
 
     virtual public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Player") && !collision.CompareTag("Bullet"))
+        if (collision.CompareTag("Bullet"))
         {
-            if(collision.CompareTag("Enemy"))
+            Bullet otherBullet = collision.gameObject.GetComponent<Bullet>();
+            if (otherBullet != null && newBulletPrefab != null)
+            {
+                Vector2 averageDirection = (direction.normalized + otherBullet.direction.normalized).normalized;
+                if(playerSource == 1)
+                    CreateNewBullet(averageDirection, speed * 2);
+
+                Destroy(gameObject);
+                Destroy(otherBullet.gameObject);
+            }
+        }
+        else if (!collision.CompareTag("Player"))
+        {
+            if (collision.CompareTag("Enemy"))
             {
                 collision.GetComponent<Enemy>().TakeDamage(damage, playerSource);
             }
             Destroy(gameObject);
         }
+    }
+
+    void CreateNewBullet(Vector2 direction, float newSpeed)
+    {
+        GameObject newBullet = Instantiate(newBulletPrefab, transform.position, Quaternion.identity);
+        Bullet bulletComponent = newBullet.GetComponent<Bullet>();
+        bulletComponent.direction = direction;
+        bulletComponent.speed = newSpeed;
+        bulletComponent.playerSource = playerSource;
+        bulletComponent.damage = damage * 2;
+
+        // Explicitly set the new bullet's velocity to ensure it moves as intended.
+        newBullet.GetComponent<Rigidbody2D>().velocity = direction * newSpeed;
     }
 }
